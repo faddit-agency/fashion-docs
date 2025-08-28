c8f86e532dc35988f6d222e710d639495f982967
donggori2025 <donggori2025@gmail.com>
2025-08-28 15:46:09 +0900

    Remove direct GA4 script to fix GTM cookie consent trigger issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,23 +39,7 @@ export default function RootLayout({
               `,
             }}
           />
-          {/* Google Analytics */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-2Y73H69QBK"
-            strategy="afterInteractive"
-          />
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-2Y73H69QBK');
-              `,
-            }}
-          />
+
         </head>
         <body className={`${inter.className} antialiased`}>
           <ThemeProvider>
